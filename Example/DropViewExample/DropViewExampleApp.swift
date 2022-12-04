3673a5140e8543c5521fd1af8990c15a6f3c50b8
Stefano Bertagno <stefano@bertagno.com>
2022-12-04 14:03:20 +0100

    chore(example): add example

---
+++ Example/DropViewExample/DropViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropViewExampleApp.swift
+//  Ciaone
+//
+//  Created by Stefano Bertagno on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct DropViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
